9aafaf49fb6b2923083dedba92c41af603633f0c
MaxBrych <max.brych03@gmail.com>
2022-12-17 11:23:31 +0100

    icon for categories in sanity studio

--- schemas/category.ts
+++ schemas/category.ts
@@ -1,19 +1,21 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from "sanity";
+import { FiTag } from "react-icons/fi";
 
 export default defineType({
-  name: 'category',
-  title: 'Category',
-  type: 'document',
+  name: "category",
+  title: "Category",
+  type: "document",
+  icon: FiTag,
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     }),
     defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      name: "description",
+      title: "Description",
+      type: "text",
     }),
   ],
-})
+});
