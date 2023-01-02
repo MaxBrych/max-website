34c8b8de8ac8f245d73abddbf753ddbf3ee2c977
MaxBrych <max.brych03@gmail.com>
2023-01-02 22:36:56 +0100

    erster publish

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,13 +3,13 @@ import { HeroSubhead } from "./HeroSubhead";
 
 export const HeroSection = () => {
   return (
-    <div className="flex-col items-center justify-center h-96 sm:align-middle sm:text-center">
+    <div className="flex-col items-center justify-center h-56 md:h-96 sm:align-middle sm:text-center">
       <section className="flex-col w-full h-full text-black text-blue-500 sm:items-center sm:justify-center sm:align-middle py-18 bg-gradient-center">
         {/*<p className="text-xl font-medium ">Hey✌️ </p>*/}
-        <h1 className="py-4 text-6xl md:text-7xl font-medium tracking-tighter ">
+        <h1 className="py-4 text-6xl font-medium tracking-tighter md:text-7xl ">
           Design. Build. Publish.
         </h1>
-        <p className="text-xl md:text-2xl">
+        <p className="text-xl text-gray-700 md:text-2xl">
           Hier steht ein kurzer einleite text
         </p>
       </section>
--- components/WorkflowSection.tsx
+++ components/WorkflowSection.tsx
@@ -8,22 +8,20 @@ export const WorkflowSection = () => {
     {
       icon: <BiPen style={icon} />,
       title: "Design",
-      descr:
-        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam ",
+      descr: "Design Systems und Prototypen erstelle ich in Figma.",
       link: "https://src.com",
     },
     {
       icon: <BiCodeAlt style={icon} />,
       title: "Build",
       descr:
-        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam ",
+        "Ich baue Websites in Webflow. Coden tuh ich mit React und Next.js.",
       link: "https://src.com",
     },
     {
       icon: <BiVolume style={icon} />,
-      title: "Publish",
-      descr:
-        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam ",
+      title: "Ship",
+      descr: "Designs veröffentliche ich auf Dribbble und Code auf Github.",
       link: "https://src.com",
     },
   ];
@@ -31,13 +29,13 @@ export const WorkflowSection = () => {
     <div className="py-6 md:px-16 md:py-14">
       <section className="md:grid md:grid-cols-3 ">
         {data.map((obj) => (
-          <div className="flex flex-col items-start justify-start gap-3 ">
+          <div className="flex flex-col items-start justify-start mb-4 ">
             {obj.icon}
             <h1 className="text-2xl font-medium">{obj.title}</h1>
             <p className="text-lg text-gray-300">{obj.descr}</p>
             {/*  <a
               href={obj.link}
-              className="px-6 py-2 mt-4 text-base font-semibold  rounded-full"
+              className="px-6 py-2 mt-4 text-base font-semibold rounded-full"
             >
               Erfahre Mehr
             </a>*/}
